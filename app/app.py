6ea8664d5662f18d33615c8c848283577f5c1a89
alexzahariadanut <alexzahariadanut@users.noreply.github.com>
2026-01-05 17:59:03 +0200

    Update app.py

--- app/app.py
+++ app/app.py
@@ -1,9 +1,14 @@
+from flask import Flask
 import socket
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return f"Hello from LightApp 🚀\nHostname: {socket.gethostname()}\n"
 
 @app.route("/health")
 def health():
     return "OK", 200
 
-@app.route("/")
-def hello():
-    return f"Hello from LightApp 🚀\nHostname: {socket.gethostname()}\n"
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
